62650aeaa5855849f4f6b227b3291fc2daededf7
karlami <karla.rivera24@gmail.com>
2019-03-23 00:36:53 -0600

    Ejemplo BNF Karla
    23.3.2019 Ejemplo de BNF, falta probarlo con la base de datos de la progra

--- BaseDeDatos.pl
+++ BaseDeDatos.pl
@@ -181,11 +181,26 @@ cuidado("Fibromialgia", "Fortaleza muscular").
 cuidado("Fibromialgia", "Controlar el estrés").
 cuidado("Fibromialgia", "Evitar la fatiga").
 
+o --> sn(_Gen2,Num), sv(Num).
+sn(Gen,Num) --> det(Gen,Num), n(Gen,Num).
+sv(Num) --> vt(Num), sn(_Gen,_Num2).
+sv(Num) --> vi(Num).
+det(f,sg) --> [la]; [una].
+det(f,pl) --> [las]; [unas].
+det(m,sg) --> [el]; [un].
+det(m,pl) --> [los]; [unos].
+vi(sg) --> [ladra].
+vi(pl) --> [ladran].
+vt(sg) --> [muerde].
+vt(pl) --> [muerden].
+n(f,sg) --> [perra].
+n(f,pl) --> [perras].
+n(m,sg) --> [perro]; [hueso].
+n(m,pl) --> [perros]; [huesos].
 
 
 
 %Porcion de mariano
-
 enfermedad("Calculos Biliales").
 sintoma("Calculos Biliales","Fiebre").
 sintoma("Calculos Biliales","Vomitos").
