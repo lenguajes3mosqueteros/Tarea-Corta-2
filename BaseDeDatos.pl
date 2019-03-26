d809cf8277f101195a41b5d1ca0a799a94f7fbb7
Mariano <marianomm@rocketmail.com>
2019-03-26 12:42:18 -0600

    Se Ordena la base de Datos

--- BaseDeDatos.pl
+++ BaseDeDatos.pl
@@ -5,181 +5,158 @@
 
 %Base de Datos de Enfermedades
 
-%Porcion de daniel
+%Enfermedades
 
 enfermedad("Diabetes").
+enfermedad("Hipertensión").
+enfermedad("Asma").
+enfermedad("Dislipidemia").
+enfermedad("Cefalea").
+enfermedad("Migraña").
+enfermedad("Lupus").
+enfermedad("Bronquitis").
+enfermedad("Osteoporosis").
+enfermedad("Fibromialgia")
+
+
+%Sintomas
 
 sintoma("Diabetes", "Aumento de sed y orina").
 sintoma("Diabetes", "Aumento del apetito").
 sintoma("Diabetes", "Fatiga").
 sintoma("Diabetes", "Vision borrosa").
 sintoma("Diabetes", "Perdida de peso").
-
-tratamiento("Diabetes", "Metformina").
-tratamiento("Diabetes", "Sulindaco").
-tratamiento("Diabetes", "Insulina").
-
-cuidado("Diabetes", "Comer sanamente").
-cuidado("Diabetes", "Hacer ejercicio").
-cuidado("Diabetes", "Revisar el azúcar en la sangre").
-cuidado("Diabetes", "Controlar el peso").
-cuidado("Diabetes", "Visitar al médico con regularidad").
-
-enfermedad("Hipertensión").
-
 sintoma("Hipertensión", "Dolor de cabeza").
 sintoma("Hipertensión", "Dificultad para respirar").
 sintoma("Hipertensión", "Sangrado nasal").
-
-tratamiento("Hipertensión", "Enapranil").
-tratamiento("Hipertensión", "Ibersartan").
-
-cuidado("Hipertensión", "Disminuir consumo de sal").
-cuidado("Hipertensión", "Hacer ejercicio").
-cuidado("Hipertensión", "Mantener un peso adecuado").
-cuidado("Hipertensión", "No beber mucho alcohol").
-
-enfermedad("Asma").
-
 sintoma("Asma", "Tos, con o sin flema").
 sintoma("Asma", "Tiraje de piel al respirar").
 sintoma("Asma", "Dificultad al respirar").
-
-tratamiento("Asma", "Salbutamol").
-tratamiento("Asma", "Ambuterol").
-tratamiento("Asma", "Salmeterol").
-tratamiento("Asma", "Bromuro de tiotropio").
-
-cuidado("Asma", "Conocer sus síntomas").
-cuidado("Asma", "Tomar lecturas").
-cuidado("Asma", "Conocer sus desencadenantes").
-cuidado("Asma", "Cuidarse al hacer ejercicio").
-
-enfermedad("Dislipidemia").
-
 sintoma("Dislipidemia", "Xantomas").
 sintoma("Dislipidemia", "Anillos opacos en el borde de la cornea").
 sintoma("Dislipidemia", "Hipertrofia del hígado o del bazo").
 sintoma("Dislipidemia", "Dificultad respiratoria").
 sintoma("Dislipidemia", "Riesgo de pancreatitis").
-
-tratamiento("Dislipidemia", "Análisis de sangre para el colesterol").
-tratamiento("Dislipidemia", "Realizar el perfil lipídico").
-tratamiento("Dislipidemia", "Uso de estaminas").
-tratamiento("Dislipidemia", "Uso de hipolipemiantes").
-
-cuidado("Dislipidemia", "Perder peso").
-cuidado("Dislipidemia", "Hacer ejercicio").
-cuidado("Dislipidemia", "Disminuir las grasas saturadas").
-cuidado("Dislipidemia", "Chequeos cada cierto tiempo").
-
-enfermedad("Cefalea").
-
 sintoma("Cefalea", "Dolor de cabeza").
 sintoma("Cefalea", "Edema palpebral").
 sintoma("Cefalea", "Espasmos faciales").
 sintoma("Cefalea", "Enrojecimiento de ojos").
 sintoma("Cefalea", "Vertigo").
-
-tratamiento("Cefalea", "Acetaminofen").
-tratamiento("Cefalea", "Antidepresivos como prevencion").
-tratamiento("Cefalea", "Analgesicos antiinflamatorios").
-
-cuidado("Cefalea", "Llevar una vida ordenada").
-cuidado("Cefalea", "Realizar ejercicio").
-cuidado("Cefalea", "Habitos de alimentacion saludables").
-cuidado("Cefalea", "Evitar el alcohol y el tabaco").
-cuidado("Cefalea", "Evitar el abuso de medicación y cafeína").
-
-%Porcion de Karla
-enfermedad("Migraña")
-
 sintoma("Migraña", "Nauseas").
 sintoma("Migraña", "Vomitos").
 sintoma("Migraña", "Hipersensibilidad a la luz").
 sintoma("Migraña", "Dolor de cabeza").
 sintoma("Migraña", "Adormecimiento de manos").
-
-tratamiento("Migraña", "Analgésicos").
-tratamiento("Migraña", "Antiinflamatorios").
-tratamiento("Migraña", "Dieta balanceada").
-tratamiento("Migraña", "Acupuntura").
-tratamiento("Migraña", "Masajes").
-
-cuidado("Migraña", "No fumar").
-cuidado("Migraña", "No beba alcohol").
-cuidado("Migraña", "Ejercicio físico").
-cuidado("Migraña", "Manejar el estrés").
-
-enfermedad("Lupus")
-
 sintoma("Lupus", "Dolor de articulaciones").
 sintoma("Lupus", "Vomitos").
 sintoma("Lupus", "Dolor muscular").
 sintoma("Lupus", "Fiebre").
 sintoma("Lupus", "Sensibilidad al sol").
-
-tratamiento("Lupus", "Antiinflamatorios no esteroideos").
-tratamiento("Lupus", "Corticoesteroides").
-tratamiento("Lupus", "Inmunodepresores").
-
-cuidado("Lupus", "No exponerse al sol").
-cuidado("Lupus", "Lavarse las manos").
-cuidado("Lupus", "Hidratarse").
-cuidado("Lupus", "Usar protector solar").
-
-enfermedad("Bronquitis")
-
 sintoma("Bronquitis", "Tos con flemas").
 sintoma("Bronquitis", "Molestia en el pecho").
 sintoma("Bronquitis", "Fatiga").
 sintoma("Bronquitis", "Fiebre").
 sintoma("Bronquitis", "Dificultad para respirar").
-
-tratamiento("Bronquitis", "Antibióticos").
-tratamiento("Bronquitis", "Antitérmicos").
-tratamiento("Bronquitis", "Inhaladores").
-tratamiento("Bronquitis", "Hidratación").
-
-cuidado("Bronquitis", "Evitar cambios bruscos de temperatura").
-cuidado("Bronquitis", "Uso de filtros purificadores").
-cuidado("Bronquitis", "Utilizar un humidificador de aire").
-cuidado("Bronquitis", "Beber mucha agua").
-
-enfermedad("Osteoporosis")
-
 sintoma("Osteoporosis", "Dolor de espalda").
 sintoma("Osteoporosis", "Pérdida de estatura").
 sintoma("Osteoporosis", "Postura encorvada").
 sintoma("Osteoporosis", "Fractura de huesos con facilidad").
-
-tratamiento("Osteoporosis", "Bifosfonatos").
-tratamiento("Osteoporosis", "Calcitonina").
-tratamiento("Osteoporosis", "Raloxifeno").
-tratamiento("Osteoporosis", "Teriparatida").
-
-cuidado("Osteoporosis", "Aporte de calcio a la dieta").
-cuidado("Osteoporosis", "Exposición al sol para vitamina D").
-cuidado("Osteoporosis", "Ejercicio físico").
-cuidado("Osteoporosis", "Evitar caídas").
-
-enfermedad("Fibromialgia")
-
 sintoma("Fibromialgia", "Dolor crónico del cuello").
 sintoma("Fibromialgia", "Dolor crónico de la espalda").
 sintoma("Fibromialgia", "Trastornos del sueño").
 sintoma("Fibromialgia", "Hipotiroidismo").
 sintoma("Fibromialgia", "Depresión").
 
+
+
+%Tratamientos
+
+tratamiento("Diabetes", "Metformina").
+tratamiento("Diabetes", "Sulindaco").
+tratamiento("Diabetes", "Insulina").
+tratamiento("Hipertensión", "Enapranil").
+tratamiento("Hipertensión", "Ibersartan").
+tratamiento("Asma", "Salbutamol").
+tratamiento("Asma", "Ambuterol").
+tratamiento("Asma", "Salmeterol").
+tratamiento("Asma", "Bromuro de tiotropio").
+tratamiento("Dislipidemia", "Análisis de sangre para el colesterol").
+tratamiento("Dislipidemia", "Realizar el perfil lipídico").
+tratamiento("Dislipidemia", "Uso de estaminas").
+tratamiento("Dislipidemia", "Uso de hipolipemiantes").
+tratamiento("Cefalea", "Acetaminofen").
+tratamiento("Cefalea", "Antidepresivos como prevencion").
+tratamiento("Cefalea", "Analgesicos antiinflamatorios").
+tratamiento("Migraña", "Analgésicos").
+tratamiento("Migraña", "Antiinflamatorios").
+tratamiento("Migraña", "Dieta balanceada").
+tratamiento("Migraña", "Acupuntura").
+tratamiento("Migraña", "Masajes").
+tratamiento("Lupus", "Antiinflamatorios no esteroideos").
+tratamiento("Lupus", "Corticoesteroides").
+tratamiento("Lupus", "Inmunodepresores").
+tratamiento("Bronquitis", "Antibióticos").
+tratamiento("Bronquitis", "Antitérmicos").
+tratamiento("Bronquitis", "Inhaladores").
+tratamiento("Bronquitis", "Hidratación").
+tratamiento("Osteoporosis", "Bifosfonatos").
+tratamiento("Osteoporosis", "Calcitonina").
+tratamiento("Osteoporosis", "Raloxifeno").
+tratamiento("Osteoporosis", "Teriparatida").
 tratamiento("Fibromialgia", "Analgésicos").
 tratamiento("Fibromialgia", "Antiinflamatorios").
 tratamiento("Fibromialgia", "Antidepresivos").
 tratamiento("Fibromialgia", "Anti convulsionantes").
 
+
+%Cuidados
+
+cuidado("Diabetes", "Comer sanamente").
+cuidado("Diabetes", "Hacer ejercicio").
+cuidado("Diabetes", "Revisar el azúcar en la sangre").
+cuidado("Diabetes", "Controlar el peso").
+cuidado("Diabetes", "Visitar al médico con regularidad").
+cuidado("Hipertensión", "Disminuir consumo de sal").
+cuidado("Hipertensión", "Hacer ejercicio").
+cuidado("Hipertensión", "Mantener un peso adecuado").
+cuidado("Hipertensión", "No beber mucho alcohol").
+cuidado("Asma", "Conocer sus síntomas").
+cuidado("Asma", "Tomar lecturas").
+cuidado("Asma", "Conocer sus desencadenantes").
+cuidado("Asma", "Cuidarse al hacer ejercicio").
+cuidado("Dislipidemia", "Perder peso").
+cuidado("Dislipidemia", "Hacer ejercicio").
+cuidado("Dislipidemia", "Disminuir las grasas saturadas").
+cuidado("Dislipidemia", "Chequeos cada cierto tiempo").
+cuidado("Cefalea", "Llevar una vida ordenada").
+cuidado("Cefalea", "Realizar ejercicio").
+cuidado("Cefalea", "Habitos de alimentacion saludables").
+cuidado("Cefalea", "Evitar el alcohol y el tabaco").
+cuidado("Cefalea", "Evitar el abuso de medicación y cafeína").
+cuidado("Migraña", "No fumar").
+cuidado("Migraña", "No beba alcohol").
+cuidado("Migraña", "Ejercicio físico").
+cuidado("Migraña", "Manejar el estrés").
+cuidado("Lupus", "No exponerse al sol").
+cuidado("Lupus", "Lavarse las manos").
+cuidado("Lupus", "Hidratarse").
+cuidado("Lupus", "Usar protector solar").
+cuidado("Bronquitis", "Evitar cambios bruscos de temperatura").
+cuidado("Bronquitis", "Uso de filtros purificadores").
+cuidado("Bronquitis", "Utilizar un humidificador de aire").
+cuidado("Bronquitis", "Beber mucha agua").
+cuidado("Osteoporosis", "Aporte de calcio a la dieta").
+cuidado("Osteoporosis", "Exposición al sol para vitamina D").
+cuidado("Osteoporosis", "Ejercicio físico").
+cuidado("Osteoporosis", "Evitar caídas").
 cuidado("Fibromialgia", "Ejercicio físico").
 cuidado("Fibromialgia", "Fortaleza muscular").
 cuidado("Fibromialgia", "Controlar el estrés").
 cuidado("Fibromialgia", "Evitar la fatiga").
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 o --> sn(_Gen2,Num), sv(Num).
 sn(Gen,Num) --> det(Gen,Num), n(Gen,Num).
