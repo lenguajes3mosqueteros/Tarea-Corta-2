e79595e364346442cacd2cf8bfabce9fb74fc81a
Mariano <marianomm@rocketmail.com>
2019-03-28 23:28:22 -0600

    Incio Jueves

--- BaseDeDatos.pl
+++ BaseDeDatos.pl
@@ -151,21 +151,3 @@ iniciar: write('Su consulta con DrLog ha iniciado, proceda a preguntarle lo que 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-o --> sn(_Gen2,Num), sv(Num).
-sn(Gen,Num) --> det(Gen,Num), n(Gen,Num).
-sv(Num) --> vt(Num), sn(_Gen,_Num2).
-sv(Num) --> vi(Num).
-det(f,sg) --> [la]; [una].
-det(f,pl) --> [las]; [unas].
-det(m,sg) --> [el]; [un].
-det(m,pl) --> [los]; [unos].
-vi(sg) --> [ladra].
-vi(pl) --> [ladran].
-vt(sg) --> [muerde].
-vt(pl) --> [muerden].
-n(f,sg) --> [perra].
-n(f,pl) --> [perras].
-n(m,sg) --> [perro]; [hueso].
-n(m,pl) --> [perros]; [huesos].
-
-
