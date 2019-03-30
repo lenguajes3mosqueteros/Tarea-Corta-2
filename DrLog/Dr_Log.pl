3be43290dfab634f807c3a4d347474256e713253
Mariano <marianomm@rocketmail.com>
2019-03-29 22:38:07 -0600

    Ultimo Update

--- DrLog/Dr_Log.pl
+++ DrLog/Dr_Log.pl
@@ -90,7 +90,11 @@ busca_enfermedad(Padecimiento,[X|T]):-
 % Lee la entrada y la convierte en una lista de terminos atomicos
 :- [readline].
 
-consulta:- write('Escribe "comenzar." para iniciar la consulta. Para terminar la consulta, escribe "adios".').
+consulta:- write('Bienvenido a Dr. Log'), nl,
+			 write('El doctor realizara una consulta, debes decirle los sintomas'), nl,
+			 write('Ademas te dara tratamiento y causas de la enfermedad'), nl,
+			 write('La consulta cuesta $500, se acepta Visa, Mastercard y AmericanExpress'), nl,
+			 write('escribe "comenzar." para iniciar la consulta. Para terminar la consulta, escribe "adios".').
 comenzar :-
 	   write('----Consulta iniciada----'), nl,
        %read(Input),
